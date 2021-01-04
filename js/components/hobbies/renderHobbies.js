3fac728b71d0d0a143c4281fc5193c4506bf6b86
Rimantas <belauzas@yahoo.com>
2021-01-04 12:55:35 +0200

    Rendering hobbies list

---
+++ js/components/hobbies/renderHobbies.js
@@ -0,0 +1,37 @@
+import { isValidHobbies } from './isValidHobbies.js';
+import { findIfValidSelector } from './findIfValidSelector.js';
+
+function renderHobbies(params) {
+    // input validation
+    if (!isValidHobbies(params)) {
+        return false;
+    }
+
+    // logic
+    const { selector, data, limit } = params;
+
+    const DOM = findIfValidSelector(selector);
+    if (!DOM) {
+        return false;
+    }
+
+    let HTML = '';
+    for (const item of data) {
+        HTML += `<div class="item">
+                    <i class="fa fa-${item.icon}"></i>
+                    <span>${item.title}</span>
+                </div>`;
+    }
+
+    // post logic validation
+
+
+
+    // result
+    DOM.classList.add('hobbie-list');
+    DOM.innerHTML = HTML;
+
+    return true;
+}
+
+export { renderHobbies }
